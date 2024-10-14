ea9af3697183e519969def167ef21b3b61233b33
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2024-10-14 12:21:53 +0300

    Update visibility of API key in api_keys.js

--- src/api_keys.js
+++ src/api_keys.js
@@ -1,3 +1,5 @@
+// This file should be added to .gitignore to keep the API key private
+// But in case of this project, the API key needs to be visible
 const apiKeys = {
     OPEN_WEATHER_MAP: "649caffec9da71ec7e992100d189c9f2"
 };
